00922c70ab4b0e264381aed857d054aba9464a10
Sebcas16131 <sebascastellanos18131@gmail.com>
2025-04-01 08:36:01 -0500

    Se realizo la creación del Home y se anexaron las imagenes correspondientes

---
+++ EXPLORE_RISARALDA/js/inicio.js
@@ -0,0 +1,35 @@
+const nav= document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click" ,() => {
+    nav.classList.add("visible");
+});
+
+cerrar.addEventListener("click", ()=>{
+    nav.classList.remove("visible");
+});
+
+
+// ir arriba
+function smoothScroll(targetY, duration) {
+    let start = window.scrollY;
+    let startTime = null;
+  
+    function animation(currentTime) {
+      if (startTime === null) startTime = currentTime;
+  
+      let timeElapsed = currentTime - startTime;
+      let progress = Math.min(timeElapsed / duration, 1);
+  
+      window.scrollTo(0, start + (targetY - start) * progress);
+  
+      if (progress < 1) requestAnimationFrame(animation);
+    }
+  
+    requestAnimationFrame(animation);
+  }
+  
+  document.getElementById("scrollBtn").addEventListener("click", () =>
+    smoothScroll(0, 1000)
+  );
