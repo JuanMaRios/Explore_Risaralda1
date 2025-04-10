bd05a3b05bcb7c5a332c6a8e8345bd28585c71ad
Sebcas16131 <sebascastellanos18131@gmail.com>
2025-04-09 19:44:13 -0500

    Detalles en el inicio y header de los municipios

--- EXPLORE_RISARALDA/js/inicio.js
+++ EXPLORE_RISARALDA/js/inicio.js
@@ -29,6 +29,23 @@ productContainer.forEach((item, i) => {
   });
 });
 
+const productContainer1 = [...document.querySelectorAll(".contenedor1")];
+const nextBtn1 = [...document.querySelectorAll(".next-btn1")];
+const preBtn1 = [...document.querySelectorAll(".pre-btn1")];
+
+productContainer1.forEach((item, i) => {
+  let dimensiones = item.getBoundingClientRect();
+  let cWidth = dimensiones.width;
+
+  nextBtn1[i].addEventListener("click", () => {
+    item.scrollLeft += cWidth;
+  });
+
+  preBtn1[i].addEventListener("click", () => {
+    item.scrollLeft -= cWidth;
+  });
+});
+
 
 
 
