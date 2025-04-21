06a5dee5876e8f7f86d595ff60b89fedf7f997f6
JuanMaRios <j.rios2@utp.edu.co>
2025-04-21 06:29:44 -0500

    Se creo y se dejo terminada con responsive la sección de detalles de paquetes

---
+++ EXPLORE_RISARALDA/js/paquetes_detalles.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuContainer = document.querySelector(".menu-container");
+    const profileIcon = document.querySelector("#p-icon");
+
+    profileIcon.addEventListener("click", () => {
+        menuContainer.classList.toggle("active");
+    });
+
+    // Cierra el menú si se hace clic fuera de él
+    document.addEventListener("click", (event) => {
+        // event.preventDefault();
+        if (!menuContainer.contains(event.target)) {
+            menuContainer.classList.remove("active");
+        }
+    });
+});
+
+// funcionalidad de notificaciones
+document.addEventListener("DOMContentLoaded", () => {
+    const notiContainer = document.querySelector(".noti-container");
+    const notiIcon = document.querySelector(".noti-icon");
+
+    notiIcon.addEventListener("click", () => {
+        notiContainer.classList.toggle("active");
+    });
+
+    // Cierra el menú si se hace clic fuera de él
+    document.addEventListener("click", (event) => {
+        // event.preventDefault();
+        if (!notiContainer.contains(event.target)) {
+            notiContainer.classList.remove("active");
+        }
+    });
+});
+
+// funcionalidad del header con responsive
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const fullscreenMenu = document.getElementById('fullscreen-menu');
+const closeIcon = document.querySelector('.fullscreen-menu .close-icon');
+
+hamburgerMenu.addEventListener('click', () => {
+    fullscreenMenu.classList.add('active');
+});
+
+closeIcon.addEventListener('click', () => {
+    fullscreenMenu.classList.remove('active');
+});
