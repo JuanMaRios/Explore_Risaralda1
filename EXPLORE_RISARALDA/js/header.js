44a7621e567ce6875767a151bff6c52ee45f0901
Lawliet26 <luisacoy2005@gmail.com>
2025-04-28 23:23:10 -0500

    cambios minimos

--- EXPLORE_RISARALDA/js/header.js
+++ EXPLORE_RISARALDA/js/header.js
@@ -1,47 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const menuContainer = document.querySelector(".menu-container");
-    const profileIcon = document.querySelector("#p-icon");
+  const menuContainer = document.querySelector(".menu-container");
+  const profileIcon = document.querySelector("#p-icon");
 
-    profileIcon.addEventListener("click", () => {
-        menuContainer.classList.toggle("active");
-    });
+  profileIcon.addEventListener("click", () => {
+    menuContainer.classList.toggle("active");
+  });
 
-    // Cierra el menú si se hace clic fuera de él
-    document.addEventListener("click", (event) => {
-        if (!menuContainer.contains(event.target)) {
-            menuContainer.classList.remove("active");
-        }
-    });
+  // Cierra el menú si se hace clic fuera de él
+  document.addEventListener("click", (event) => {
+    if (!menuContainer.contains(event.target)) {
+      menuContainer.classList.remove("active");
+    }
+  });
 });
 
 // funcionalidad de notificaciones
 document.addEventListener("DOMContentLoaded", () => {
-    const notiContainer = document.querySelector(".noti-container");
-    const notiIcon = document.querySelector(".noti-icon");
+  const notiContainer = document.querySelector(".noti-container");
+  const notiIcon = document.querySelector(".noti-icon");
 
-    notiIcon.addEventListener("click", () => {
-        notiContainer.classList.toggle("active");
-    });
+  notiIcon.addEventListener("click", () => {
+    notiContainer.classList.toggle("active");
+  });
 
-    // Cierra el menú si se hace clic fuera de él
-    document.addEventListener("click", (event) => {
-        if (!notiContainer.contains(event.target)) {
-            notiContainer.classList.remove("active");
-        }
-    });
+  // Cierra el menú si se hace clic fuera de él
+  document.addEventListener("click", (event) => {
+    if (!notiContainer.contains(event.target)) {
+      notiContainer.classList.remove("active");
+    }
+  });
 });
 
 // funcionalidad del header con responsive
 
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const fullscreenMenu = document.getElementById('fullscreen-menu');
-const closeIcon = document.querySelector('.fullscreen-menu .close-icon');
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const fullscreenMenu = document.getElementById("fullscreen-menu");
+const closeIcon = document.querySelector(".fullscreen-menu .close-icon");
 
-hamburgerMenu.addEventListener('click', () => {
-    fullscreenMenu.classList.add('active');
+hamburgerMenu.addEventListener("click", () => {
+  fullscreenMenu.classList.add("active");
 });
 
-closeIcon.addEventListener('click', () => {
-    fullscreenMenu.classList.remove('active');
+closeIcon.addEventListener("click", () => {
+  fullscreenMenu.classList.remove("active");
 });
+
+
 
